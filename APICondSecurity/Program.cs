8c79ab51067a1edc8a919017f6cd9e279d6ca44a
jussantxtecnologia <jussan.silva@txtecnologia.com.br>
2024-06-12 19:57:53 -0300

    Ajustes Swagger

--- APICondSecurity/Program.cs
+++ APICondSecurity/Program.cs
@@ -51,11 +51,10 @@ builder.Services.AddScoped<VeiculoTerceiroRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 
